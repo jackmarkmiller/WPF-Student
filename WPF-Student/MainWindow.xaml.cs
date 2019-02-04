2a083ddc43404f55ecb7499ea907d3d88998ba49
Jack Markmiller <jackmarkmiller@ou.edu>
2019-02-04 11:33:41 -0600

    WPF In Class 2/4/2019

--- WPF-Student/MainWindow.xaml.cs
+++ WPF-Student/MainWindow.xaml.cs
@@ -20,9 +20,37 @@ namespace WPF_Student
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Student> students = new List<Student>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string idAsString = id1.Text;
+            Student a = new Student();
+            a.SoonerID  = Convert.ToInt32(idAsString);
+            a.FirstName = name.Text;
+            a.GPA = Convert.ToDouble(gpa.Text);
+            students.Add(a);
+            var futureDate = Convert.ToDateTime("1/15/2020");
+            DateTime studentDOB = Convert.ToDateTime(dob.Text);
+            var x = studentDOB.AddDays(50);
+            DateTime rightNow = DateTime.Now;
+            var diff = rightNow - studentDOB;
+
+            // MessageBox.Show($"Hello {fullName}, {studentID} with a GPA of {gpa}");
+
+            output.Text = $"Hello {a.FirstName} ({a.SoonerID})";
+
+            
         }
     }
 }
